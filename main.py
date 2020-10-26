64cf43513e6875d05f349c355bafc2c374048928
Alex Wolfe <3718652+afwolfe@users.noreply.github.com>
2020-10-26 13:31:41 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,36 @@
+"""
+RoomArea
+Author:
+
+Original assignment by: Edhesive Intro to CS
+
+Find the area of an irregularly shaped room with the shape as shown in room.png.
+
+Ask the user to enter the values for sides A, B, C, D, and E and print out the total room area.
+
+"""
+
+def right_triangle_area(base, height):
+  """
+  
+  """
+
+  return 0
+
+def rectangle_area(length, width):
+  """
+  
+  """
+
+  return 0
+
+def room_area():
+  """
+  This function asks for the user inputs and then calculates and returns the area of the room.
+  
+  """
+
+  return 0
+
+final_area = room_area()
+print("Room area: " + str(final_area))
