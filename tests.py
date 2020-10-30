5c858535bdfa736c1ce18d5792573565bbde6f0e
Alex Wolfe <3718652+afwolfe@users.noreply.github.com>
2020-10-30 14:51:52 -0400

    Added additional test cases.

--- tests.py
+++ tests.py
@@ -27,4 +27,25 @@ class RoomAreaTest(TestCase):
     def test_random(self):
         """This test tests a random set of values."""
         a,b,c,d,e = [round(random()*randint(1,50),2) for x in range(5)]
-        self.assertAlmostEqual(a*d-0.5*a*e+b*c-c*d+0.5*c*e, main.room_area(a,b,c,d,e),2,"Random values failed the test: {}".format([a,b,c,d,e]))
+        answer = main.room_area(a,b,c,d,e)
+        self.assertAlmostEqual(answer, a*(d-0.5*e)+c*(b-d+0.5*e),2,"Random values failed the test: {}".format([a,b,c,d,e]))
+
+    def test_square(self):
+        """This test tests a square room (a=b=c=d, e=0)"""
+        length = randint(1,50)
+        answer = main.room_area(length,length,length,length,0)
+        self.assertEqual(answer, length**2, "If a,b,c,d are all equal and e is 0, the area should just be one large square.")
+
+    def test_right_triangle_area(self):
+        """This directly tests the right triangle function."""
+        b = randint(1,50)
+        h = randint(1,50)
+        answer = main.right_triangle_area(b,h)
+        self.assertEqual(answer, 0.5*b*h, "Your right_triangle_area function does not return the correct area.")
+    
+    def test_rectangle_area(self):
+        """This directly tests the square_area function."""
+        w = randint(1,50)
+        h = randint(1,50)
+        answer = main.rectangle_area(w,h)
+        self.assertEqual(answer, w*h, "Your rectangle_area function does not return the correct area.")
